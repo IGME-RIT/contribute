6e6ae73b8822f7cd9751ec5c0916fa50b4fc579c
David Erbelding <dje3873@rit.edu>
2016-12-15 12:06:54 -0500

    add base projects

---
+++ baseProjects/OpenGL Base/README.md
@@ -0,0 +1,9 @@
+# [Example Project]
+
+[Put a more in depth explanation of the project here]
+
+# Setup
+
+You will need to have CMake installed on your computer, and properly added to your path.
+Windows setup has been configured for use with Visual Studio.
+Running setup.cmd will create a visual studio project in your build folder.
---
+++ baseProjects/OpenGL Base/source/main.cpp
@@ -0,0 +1,79 @@
+﻿/*
+Example Project
+(c) 2016
+Author: David Erbelding
+Written under the supervision of David I. Schwartz, Ph.D., and
+supported by a professional development seed grant from the B. Thomas
+Golisano College of Computing & Information Sciences
+(https://www.rit.edu/gccis) at the Rochester Institute of Technology.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+// Window resize callback
+void resizeCallback(GLFWwindow* window, int width, int height)
+{
+	// do something
+}
+
+// This will get called when the mouse moves.
+void mouseMoveCallback(GLFWwindow *window, GLdouble mouseX, GLdouble mouseY)
+{
+	// do something
+}
+
+// Change settings when keys are pressed
+void keyPressCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_R && action == GLFW_RELEASE)
+    {
+		// do something
+    }
+}
+
+int main(int argc, char **argv)
+{
+	// Initializes the GLFW library
+	glfwInit();
+
+	// Initialize window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Example Project", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	// Common callback functions
+	glfwSetFramebufferSizeCallback(window, resizeCallback);
+    glfwSetCursorPosCallback(window, mouseMoveCallback);
+    glfwSetKeyCallback(window, keyPressCallback);
+
+	// Initializes the glew library
+	glewInit();
+	
+	/* Begin Example Code Here */
+	
+	
+	
+	
+	
+	/* End Example Code Here*/
+
+	// Free GLFW memory.
+	glfwTerminate();
+	// End of Program.
+	return 0;
+}
